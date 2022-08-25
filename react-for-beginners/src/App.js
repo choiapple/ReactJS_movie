6321ce56dedc11007837daff16700fb71d1a6adf
choiapple <79374648+choiapple@users.noreply.github.com>
2022-08-25 20:12:51 +0900

    8월 25일 last

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -39,37 +39,69 @@
 
 // export default App;
 
-import React from 'react';
-import { useEffect, useState } from 'react';
-function Hello(){
-  // function byFn(){
-  //   console.log("bye :(");
-  // }
-  // function hiFn(){
-  //   console.log("create :)");
-  //   return byFn;
-  // }
-  // useEffect(hiFn, []);
-  useEffect(function (){
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  },[]);
-  useEffect(()=>{
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  },[]);
-  return <h1>Hello</h1>
-}
+// import React from 'react';
+// import { useEffect, useState } from 'react';
+// function Hello(){
+//   // function byFn(){
+//   //   console.log("bye :(");
+//   // }
+//   // function hiFn(){
+//   //   console.log("create :)");
+//   //   return byFn;
+//   // }
+//   // useEffect(hiFn, []);
+//   useEffect(function (){
+//     console.log("hi :)");
+//     return function () {
+//       console.log("bye :(");
+//     };
+//   },[]);
+//   useEffect(()=>{
+//     console.log("hi :)");
+//     return () => console.log("bye :(");
+//   },[]);
+//   return <h1>Hello</h1>
+// }
 
-function App(){
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
+// function App(){
+//   const [showing, setShowing] = useState(false);
+//   const onClick = () => setShowing(prev => !prev);
+//   return (
+//     <div>
+//       {showing ? <Hello />:null}
+//       <button onClick={onClick}>{showing ? "Hide":"Show"}</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if(toDo === ""){
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello />:null}
-      <button onClick={onClick}>{showing ? "Hide":"Show"}</button>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+      <input 
+      onChange={onChange}
+      value={toDo} 
+      type="text" 
+      placeholder="Write your to do..." 
+      />
+      <button>Add To Do</button>
+      </form>
     </div>
   );
 }
